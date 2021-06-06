4e5c4fa0c901acdbb90efee386fd62ea0d4785a2
Chris DeRose <chris@chrisderose.com>
2021-06-06 18:47:09 -0400

    Switched from viewnior to feh. Added feh config. Added shadows to Chrom windows

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -7,4 +7,4 @@ FILENAME="Screenshot "$(/usr/bin/date "+%Y-%m-%d %I:%M:%S")".png"
 # bash -c "sleep 0.25 && scrot -q 90 -s -e 'mv \$f ~/Pictures/Screenshots/'"
 
 /usr/bin/maim -s -o | /usr/bin/convert png:- \( -clone 0 -background black -shadow 80x3+5+5 \) +swap -background none -layers merge +repage "$DIR_OUTPUT/$FILENAME"
-/usr/bin/viewnior "$DIR_OUTPUT/$FILENAME"
+/usr/bin/feh "$DIR_OUTPUT/$FILENAME"
