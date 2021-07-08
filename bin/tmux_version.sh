3b4007bfda21fb99864153b5bf6b606e4fd751fc
Chris DeRose <chris@chrisderose.com>
2021-07-07 21:59:17 -0400

    Added if statements to the tmux to control syntax based on version

---
+++ bin/tmux_version.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ECHO="/usr/bin/echo"
+SED="/usr/bin/sed"
+BC="/usr/bin/bc"
+TMUX="/usr/bin/tmux"
+
+if [ ! $# -eq 2 ]; then
+  echo "Invalid Parameters. Two arguments are required."
+  exit 2
+fi
+
+if [[ $1 == 'lt' ]]; then
+  operator="<"
+elif [[ $1 == 'lte' ]]; then
+  operator="<="
+elif [[ $1 == 'gt' ]]; then
+  operator=">"
+elif [[ $1 == 'gte' ]]; then
+  operator=">="
+elif [[ $1 == 'eq' ]]; then
+  operator="=="
+else
+  echo "Parameter one invalid. Must be one of: lt, lte, gt, gte, eq."
+  exit 2
+fi
+
+if [[ $2 =~ ^[0-9\.]+$ ]]; then
+  version_right=$2
+else
+  echo "Parameter two invalid. Must be numeric."
+  exit 2
+fi
+
+version_left=$($TMUX -V | $SED 's/[^0-9\.]*//g')
+
+if [ $($ECHO "$version_left $operator $version_right" | $BC) = 1 ]; then
+  exit 0
+fi
+
+exit 1
