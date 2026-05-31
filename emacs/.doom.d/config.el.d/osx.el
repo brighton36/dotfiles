52cab15e038f1b7deb9f7481c301e8f64774222c
Chris.DeRose <cderose@derosetechnologies.com>
2026-05-31 18:41:54 -0400

    lots of little keybinding changes on osx

--- emacs/.doom.d/config.el.d/osx.el
+++ emacs/.doom.d/config.el.d/osx.el
@@ -3,6 +3,9 @@
 ; Native OSX full screen
 (setq ns-use-native-fullscreen t)
 
+; Hide the titlebar:
+(add-to-list 'default-frame-alist '(undecorated . t))
+
 ; GPG seems to need this
 (setenv "GPG_AGENT_INFO" nil)
 (setq epg-gpg-program "/opt/homebrew/opt/gnupg@2.2/bin/gpg")
